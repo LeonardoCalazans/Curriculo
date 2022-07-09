44aefc0994f3399b1878cd9fc39c4a4fc6a6c602
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-07-09 20:36:45 -0300

    fix: adding smoothScroll in button goTop

--- javascript/goHome.js
+++ javascript/goHome.js
@@ -4,6 +4,9 @@ window.onscroll = () => {
 
 const scroll = () => {
     const backTop = document.getElementById('goHome');
+    backTop.addEventListener('click', () => {
+        smoothScrollTo(0, 0, 500);
+    });
     if (window.pageYOffset < 50) {
         backTop.style.display = 'none';
         backTop.style.animation = 'animation 2s';
